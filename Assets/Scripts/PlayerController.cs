ab2e7d8ccd1338e19c613031839615f89ef9ffec
vmetric <armada2526pbem@gmail.com>
2018-07-04 17:26:31 -0600

    Added count of pickups and win text
    Added a counter that tracks how many pickups have been picked up, as well as text that displays once all pickups have been picked up.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
+	private int count;
 
 	void Start () 
 	{
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 		
     void FixedUpdate ()
@@ -27,6 +34,16 @@ public class PlayerController : MonoBehaviour {
 	{
 		if (other.gameObject.CompareTag("Pickup")) {
 			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You win!";
 		}
 	}
 }
